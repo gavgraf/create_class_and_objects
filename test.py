d87dd65d1e69f8f4dd5d37cca8d2a05a5e60d07e
Артем GAV <robockop_12@mail.ru>
2025-02-10 19:27:08 +0300

    Update vacation v0.2

--- test.py
+++ test.py
@@ -4,6 +4,22 @@ class Phone:
     def __init__(self, dial_type_value):
         self.dial_type = dial_type_value
 
+    def ring(self):
+        print('Дзззззыыыыыыыынь')
+
+    def call(self, phone_number):
+        # Сначала в вывод подставляется значение параметра phone_number,
+        # а затем - атрибута класса Phone.
+        print(f'Звоню по номеру {phone_number}! Тип связи - {self.line_type}.')
+
+    def get_missed_calls(self):
+        print('Запрос количества пропущенных вызовов')
+
 
 rotary_phone = Phone(dial_type_value='дисковый')
+rotary_phone.call('555-2368')
+# Вызов метода call(). Передаётся аргумент '555-2368'
+# для параметра phone_number.
 print(f'Тип набора: {rotary_phone.dial_type}.')
+rotary_phone.ring()
+rotary_phone.get_missed_calls()
--- vacation.py
+++ vacation.py
@@ -1,39 +1,26 @@
 class Employee:
     vacation_days = 28
 
-    def __init__(self,
-                 first_name_value,
-                 second_name_value,
-                 gender_value
-                 ):
-
+    def __init__(self, first_name_value, second_name_value, gender_value):
         self.first_name = first_name_value
         self.second_name = second_name_value
         self.gender = gender_value
+        self.remaining_vacation_days = self.vacation_days
+
+    def consume_vacation(self, consume_value):
+        self.remaining_vacation_days = self.remaining_vacation_days - consume_value
+
+    def get_vacation_details(self):
+        return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
 
 
-employee1 = Employee(
-    first_name_value='Роберт',
-    second_name_value='Крузо',
-    gender_value='м'
-    )
-
-employee2 = Employee(
-    first_name_value='Роберт',
-    second_name_value='Крузо',
-    gender_value='м'
-    )
+employee = Employee('Роберт', 'Крузо', 'м')
+employee.consume_vacation(7)
 
 print(
-    f'Имя: {employee1.first_name}, '
-    f'Фамилия: {employee1.second_name}, '
-    f'Пол: {employee1.gender}, '
-    f'Отпускных дней в году: {employee1.vacation_days}.'
+    f'Имя: {employee.first_name}, \
+    Фамилия: {employee.second_name}, \
+    Пол: {employee.gender}, \
+    Отпускных дней в году: {employee.vacation_days}.'
 )
-
-print(
-    f'Имя: {employee2.first_name}, '
-    f'Фамилия: {employee2.second_name}, '
-    f'Пол: {employee2.gender}, '
-    f'Отпускных дней в году: {employee2.vacation_days}.'
-)
+print(employee.get_vacation_details())
